498a8f768668559fe74373afe9ecf6590d3456cd
FiorellaQA <fiorella_90_17@hotmail.com>
2017-05-28 19:48:31 -0500

    refactorizando código y añadiendo readme.md

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,11 +1,16 @@
-
+var map = {};
+var latitud, longitud, miUbicacion;
+var inputPartida = document.getElementById("input-origen");
+var inputDestino = document.getElementById("input-destino");
 
 function initMap() {
   var laboratoriaLima = {lat: -12.1191427, lng: -77.0349046};
 
-  var map = new google.maps.Map(document.getElementById('map'), {
+  map = new google.maps.Map(document.getElementById('map'), {
     zoom: 13,
-    center: laboratoriaLima
+    center: laboratoriaLima,
+    // mapTypeId: google.maps.MapTypeId.TERRAIN,
+    disableDefaultUI: true
   });
 
   // var markerLaboratoria = new google.maps.Marker({
@@ -13,9 +18,60 @@ function initMap() {
   //   map: map
   // });
 
-  var latitud, 
-      longitud, 
-      miUbicacion;
+    //-------- Añadiendo autocompletado--//
+  new google.maps.places.Autocomplete(inputPartida);
+  new google.maps.places.Autocomplete(inputDestino);
+
+  map.setOptions({styles: [{"featureType":"administrative","elementType":"all","stylers":[{"visibility":"on"},{"lightness":33}]},{"featureType":"landscape","elementType":"all","stylers":[{"color":"#f2e5d4"}]},{"featureType":"poi.park","elementType":"geometry","stylers":[{"color":"#c5dac6"}]},{"featureType":"poi.park","elementType":"labels","stylers":[{"visibility":"on"},{"lightness":20}]},{"featureType":"road","elementType":"all","stylers":[{"lightness":20}]},{"featureType":"road.highway","elementType":"geometry","stylers":[{"color":"#c5c6c6"}]},{"featureType":"road.arterial","elementType":"geometry","stylers":[{"color":"#e4d7c6"}]},{"featureType":"road.local","elementType":"geometry","stylers":[{"color":"#fbfaf7"}]},{"featureType":"water","elementType":"all","stylers":[{"visibility":"on"},{"color":"#acbcc9"}]}]});
+  
+  google.maps.event.addListener(map, 'click', function(event) {
+    var marker = new google.maps.Marker({
+      position: event.latLng,
+      map: map
+    });
+   });
+}
+ 
+// --------Trazando Ruta------//
+document.getElementById("form1").addEventListener("submit", function(e) {
+  e.preventDefault();
+
+  var directionsService = new google.maps.DirectionsService;
+  var directionsDisplay = new google.maps.DirectionsRenderer;
+    
+  directionsService.route({
+      origin: inputPartida.value,
+      destination: inputDestino.value,
+      travelMode: 'DRIVING'
+      }, function(response, status){
+        if(status === 'OK'){
+          var tarifa = document.getElementById("tarifa");
+          var distancia = Number((response.routes[0].legs[0].distance.text.replace("km","")).replace(",","."));
+          tarifa.classList.remove("none");
+
+          var costo = distancia*1.75;
+          console.log(costo);
+
+          if (costo < 4){
+            tarifa.innerHTML="S/. 4";
+          }else{
+            tarifa.innerHTML="S/. " + parseInt(costo);
+            console.log(response.routes[0].legs[0].distance.text);
+          }
+
+          directionsDisplay.setDirections(response);
+          // miUbicacion.setMap(null);
+        }else{
+          window.alert("No encontramos una ruta.");
+        }
+      });
+  directionsDisplay.setMap(map)
+});
+// --------Ubicandome------//
+document.getElementById("encuentrame").addEventListener("click",function(){
+  if(navigator.geolocation){
+    navigator.geolocation.getCurrentPosition(funcionExito, funcionError);
+  }
 
   function funcionExito(posi){
     latitud = posi.coords.latitude;
@@ -33,67 +89,8 @@ function initMap() {
   function funcionError(error){
     alert("Tenemos un problema con encontrar tu ubicación");
   }
+});
 
-  function buscar(){
-    if(navigator.geolocation){
-      navigator.geolocation.getCurrentPosition(funcionExito, funcionError);
-    }
-  }
-
-  document.getElementById("encuentrame").addEventListener("click",buscar);
-
-  //-------- Añadiendo autocompletado (debe estar dentro de la función initMap)
-  var inputPartida = document.getElementById("input-origen");
-  var inputDestino = document.getElementById("input-destino");
-
-  new google.maps.places.Autocomplete(inputPartida);
-  new google.maps.places.Autocomplete(inputDestino);
-  
-  // --------Trazando Ruta------//
-  var directionsService = new google.maps.DirectionsService;
-  var directionsDisplay = new google.maps.DirectionsRenderer;
-
-  function calculateAndDisplayRoute(directionsService, directionsDisplay){
-    directionsService.route({
-      origin: inputPartida.value,
-      destination: inputDestino.value,
-      travelMode: 'DRIVING'
-    }, function(response, status){
-        if(status === 'OK'){
-          var tarifa = document.getElementById("tarifa");
-          var distancia = Number((response.routes[0].legs[0].distance.text.replace("km","")).replace(",","."));
-          tarifa.classList.remove("none");
-
-          var costo = distancia*1.75;
-          console.log(costo);
-
-          if (costo < 4){
-            tarifa.innerHTML="S/. 4";
-          }else{
-            tarifa.innerHTML="S/. " + parseInt(costo);
-            console.log(response.routes[0].legs[0].distance.text);
-          }
-
-          directionsDisplay.setDirections(response);
-          miUbicacion.setMap(null);
-
-        }else{
-          window.alert("No encontramos una ruta.");
-        }
-    });
-  }
-
-  directionsDisplay.setMap(map); 
-
-  function trazarRuta(){
-    calculateAndDisplayRoute(directionsService, directionsDisplay);
-  }
-
-  document.getElementById("form1").addEventListener("submit", function(e) {
-    e.preventDefault();
-    trazarRuta();
-  });
-}
 
 
 
